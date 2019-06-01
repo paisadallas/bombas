254c28205ce00b94ac9eaf2b38612a1fa33030d7
paisadallas <44854381+paisadallas@users.noreply.github.com>
2019-06-01 10:58:10 -0500

    DisparadorListo
    Proyectil esta listo para ser lanzado

--- Bombas/Assets/Scripts/Tablero1/Proyectil.cs
+++ Bombas/Assets/Scripts/Tablero1/Proyectil.cs
@@ -6,32 +6,43 @@ public class Proyectil : MonoBehaviour
 {
    
     private Rigidbody2D rb2d;
+    private Rigidbody2D slingRb;
     private SpringJoint2D sj2d;
     private bool press;
     private float relaseDealy;
+    private float disMax = 2f;
+    
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         sj2d = GetComponent<SpringJoint2D>();
+        slingRb = sj2d.connectedBody;
+        relaseDealy = 1 / (sj2d.frequency * 4);   // separar objeto en un tiempo x
 
-        relaseDealy = 1 / (sj2d.frequency * 4);
     }
     void Update()
-    {
-     
+    {                   
         if (press)
         {
             arrastrarBola();  
-        }
-
-                
+        }                       
     }
 
     private void arrastrarBola()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float distance = Vector2.Distance(mousePosition, slingRb.position);
         rb2d.position = mousePosition;
+
+        if(distance> disMax) {
+            Vector2 direction = (mousePosition - slingRb.position).normalized;
+            rb2d.position = slingRb.position + direction * disMax;
+        }
+        else
+        {
+
+        }
     }
 
     private void OnMouseDown()
