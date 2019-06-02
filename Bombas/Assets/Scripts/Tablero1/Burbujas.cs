83c3db22667c8f859b8c9ad7a27f2807f5e99258
paisadallas <44854381+paisadallas@users.noreply.github.com>
2019-06-01 23:37:34 -0500

    Contador
    Clase contador en un 30%

--- Bombas/Assets/Scripts/Tablero1/Burbujas.cs
+++ Bombas/Assets/Scripts/Tablero1/Burbujas.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Burbujas : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    private BoxCollider2D bc2d;
+    private Rigidbody2D rb2dItem;
+    public GameObject ItemInterior;
+    [Range(1, 5)]
+    public int resistencia;
+
+    private void Awake()
+    {
+         
+    }
+   
     void Start()
     {
-        
+        resistencia = 4; 
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bc2d = GetComponent<BoxCollider2D>();
+        rb2dItem = ItemInterior.GetComponent<Rigidbody2D>();
+        resistencia = resistencia - 1;
+        Debug.Log(resistencia); 
+        if (resistencia <= 0)
+        {
+            bc2d.enabled = false;
+            rb2dItem.bodyType = RigidbodyType2D.Dynamic;
+            
+        }
+    }
+
+
+
 }
