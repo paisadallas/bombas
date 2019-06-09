bcc5c893f6bfa2d58f0b315a5b56bf92accdc0fb
paisadallas <44854381+paisadallas@users.noreply.github.com>
2019-06-09 03:38:37 -0500

    Obstaculos al 40%
    se corrigio pos Incial Canvas

---
+++ Bombas/Assets/Scripts/Tablero1/TableroUno.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableroUno : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 50, 30), "Reset"))
+                
+            Application.LoadLevel(Application.loadedLevel);
+        
+    }
+}
